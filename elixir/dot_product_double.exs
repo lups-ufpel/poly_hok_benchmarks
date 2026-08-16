2a8d7795d15a9573db31b2cbd6eb812312df7fa9
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-08-16 14:22:19 -0300

    checked dp double

--- elixir/dot_product_double.exs
+++ elixir/dot_product_double.exs
@@ -1,7 +1,7 @@
 require PolyHok
 
 PolyHok.defmodule DP do
-  include(CAS_Poly)
+  include(OpenCL_CAS_Poly)
 
   defk map_2kernel(a1, a2, a3, size, f) do
     id = blockIdx.x * blockDim.x + threadIdx.x
@@ -15,7 +15,7 @@ PolyHok.defmodule DP do
     {l, c} = PolyHok.get_shape_gnx(t1)
     type = PolyHok.get_type_gnx(t2)
     size = l * c
-    result_gpu = PolyHok.new_gnx(l, c, type)
+    result_gpu = PolyHok.new_gnx({l, c}, type)
 
     threadsPerBlock = 256
     numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
