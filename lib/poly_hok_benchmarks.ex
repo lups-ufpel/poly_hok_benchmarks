70fcd858ec78733a4ce02f98ec798035d8c4c641
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-08-15 21:49:46 -0300

    Upload initial project

---
+++ lib/poly_hok_benchmarks.ex
@@ -0,0 +1,18 @@
+defmodule PolyHokBenchmarks do
+  @moduledoc """
+  Documentation for `PolyHokBenchmarks`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PolyHokBenchmarks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poly_hok_benchmarks_test.exs
@@ -0,0 +1,8 @@
+defmodule PolyHokBenchmarksTest do
+  use ExUnit.Case
+  doctest PolyHokBenchmarks
+
+  test "greets the world" do
+    assert PolyHokBenchmarks.hello() == :world
+  end
+end
